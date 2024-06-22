bcdf7d08c9a513f69d5138b3973dc2aa51077d79
anadiasDN <162807075+anadiasDN@users.noreply.github.com>
2024-06-22 17:53:42 -0300

    Update MySQLConnector.java

--- lojadecroche/src/main/java/lojacroche/util/MySQLConnector.java
+++ lojadecroche/src/main/java/lojacroche/util/MySQLConnector.java
@@ -5,17 +5,17 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class MySQLConnector {
-    // Configurações de conexão
+    
     private static final String URL = "jdbc:mariadb://localhost:3306/lojadcroche";
     private static final String USER = "root";
     private static final String PASSWORD = "";
 
-    // Método para obter uma conexão com o banco de dados
+    
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 
-    // Método para fechar a conexão com o banco de dados
+    
     public static void closeConnection(Connection connection) {
         if (connection != null) {
             try {
